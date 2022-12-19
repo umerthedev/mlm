948c65db33bf02554147acd411e5f64eb322454b
A Mahmud Umer <94960783+umerthedev@users.noreply.github.com>
2022-12-20 00:41:21 +0600

    home page and login page added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\HomeController;
+use App\Http\Controllers\User\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//HomeRoute
+Route::get('/',[HomeController::class,'index'])->name('user.home');
+
+//LoginRoute
+Route::get('/login',[LoginController::class,'index'])->name('user.login');
+
+
